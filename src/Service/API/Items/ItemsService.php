ecd63b7dfde37483ef8a9aaeb06ba005ca2d4e8b
!AC4G ʕ•́ᴥ•̀ʔっ <76955327+AC4GPutin@users.noreply.github.com>
2022-04-10 00:08:06 +0200

    get item by id or creater added

--- src/Service/API/Items/ItemsService.php
+++ src/Service/API/Items/ItemsService.php
@@ -2,7 +2,49 @@
 
 namespace App\Service\API\Items;
 
+use App\Entity\User;
+use App\Entity\Item;
+use App\Repository\UserRepository;
+use App\Repository\ItemRepository;
+
 class ItemsService
 {
+    public function __construct(
+        private ItemRepository $itemRepository,
+        private UserRepository $userRepository
+    )
+    {
+    }
 
+    public function getItems(): ?array
+    {
+        return $this->itemRepository->findAll();
+    }
+
+    public function getItemDependentOnProperty(
+        string $property
+    ): ?Item
+    {
+        if (is_numeric($property)) {
+            return $this->itemRepository->findOneBy(['id' => (int)$property]);
+        }
+
+        $user = $this->userRepository->findOneBy(['nickname' => $property]);
+
+        if (is_null($user)) {
+            return null;
+        }
+
+        return $this->itemRepository->findOneBy(['user' => $user]);
+    }
+
+    public function updateItem(
+        string $property,
+        array $parameter
+    ): ?Item
+    {
+        //TODO: update script with lot of foreach:)
+
+        return null;
+    }
 }

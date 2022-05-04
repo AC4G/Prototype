1cc53383f77d2909bd42de890e93460856191a9f
!AC4G ʕ•́ᴥ•̀ʔっ <76955327+AC4GPutin@users.noreply.github.com>
2022-05-04 17:13:50 +0200

    rebuild InventoriesService and InventoryController

--- src/Service/API/Inventories/InventoriesService.php
+++ src/Service/API/Inventories/InventoriesService.php
@@ -2,20 +2,18 @@
 
 namespace App\Service\API\Inventories;
 
+use App\Entity\User;
+use App\Entity\Item;
 use App\Entity\Inventory;
 use App\Service\DataService;
-use App\Repository\ItemRepository;
 use App\Repository\UserRepository;
 use App\Repository\InventoryRepository;
 
 class InventoriesService
 {
-    private array $message = [];
-
     public function __construct(
         private InventoryRepository $inventoryRepository,
         private UserRepository $userRepository,
-        private ItemRepository $itemRepository,
         private DataService $dataService
     )
     {
@@ -38,25 +36,9 @@ class InventoriesService
 
     public function updateInventory(
         array $parameter,
-        string $property
+        Inventory $inventory
     )
     {
-        $validation = $this->validateData($parameter);
-
-        if (is_null($validation)) {
-            return;
-        }
-
-        $data = $this->getUserAndItem($parameter, $property);
-
-        $inventory = $this->inventoryRepository->findOneBy(['user' => $data['user'], 'item' => $data['item']]);
-
-        if (is_null($inventory)) {
-            $this->message['inventory'] = 'User does not has this item. Please use POST method to add item!';
-            return;
-        }
-
-
         if (array_key_exists('amount', $parameter)) {
             $inventory->setAmount($inventory->getAmount() + $parameter['amount']);
         }
@@ -91,72 +73,21 @@ class InventoriesService
 
     public function createEntryInInventory(
         array $parameter,
-        string $property
+        User $user,
+        Item $item
     )
     {
-        $validation = $this->validateData($parameter);
-
-        if (is_null($validation)) {
-            return;
-        }
-
-        $data = $this->getUserAndItem($parameter, $property);
-
         $inventory = new Inventory();
 
         $inventory
-            ->setUser($data['user'])
-            ->setItem($data['item'])
+            ->setUser($user)
+            ->setItem($item)
             ->setAmount($parameter['amount'])
             ->setParameter((array_key_exists('parameter', $parameter) ? $parameter['parameter'] : '{}'))
         ;
 
         $this->inventoryRepository->persistEntity($inventory);
         $this->inventoryRepository->flushEntity();
-    }
-
-    private function validateData(
-        array $parameter
-    ): ?bool
-    {
-        if (!array_key_exists('itemId', $parameter)) {
-            $this->message['itemId'] = 'JSON not contain itemId from item';
-
-            return null;
-        }
-
-        if (!array_key_exists('amount', $parameter) && !array_key_exists('parameter', $parameter)) {
-            $this->message['amount'] = 'JSON not contain amount of items and parameter. On of them are necessary!';
-
-            return null;
-        }
-
-        return true;
-    }
-
-    private function getUserAndItem(
-        array $parameter,
-        string $property
-    ): ?array
-    {
-        $user = $this->userRepository->findOneBy((is_numeric($property) ? ['id' => (int)$property] : ['nickname' => $property]));
-
-        if (is_null($user)) {
-            $this->message['user'] = 'User not exists!';
-            return null;
-        }
-
-        $item = $this->itemRepository->findOneBy(['id' => $parameter['itemId']]);
-
-        if (is_null($item)) {
-            $this->message['item'] = sprintf('Item with id %s do not exists', $parameter['itemId']);
-            return null;
-        }
-
-        return [
-            'user' => $user,
-            'item' => $item
-        ];
     }
 
     public function prepareInventories(
@@ -179,16 +110,6 @@ class InventoriesService
         return $this->dataService->removeProperties($data, [
             'id'
         ]);
-    }
-
-    public function hasMessages(): bool
-    {
-        return count($this->message) > 0;
-    }
-
-    public function getMessages(): array
-    {
-        return $this->message;
     }
 
 

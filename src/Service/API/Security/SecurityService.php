765e1352c26d49b326faf0d015394c7fdea22c0a
AC4G <vost_400@gmx.de>
2022-06-19 19:11:10 +0200

    client credentials grant added

--- src/Service/API/Security/SecurityService.php
+++ src/Service/API/Security/SecurityService.php
@@ -2,32 +2,49 @@
 
 namespace App\Service\API\Security;
 
+use DateTime;
+use Firebase\JWT\JWT;
 use App\Entity\Client;
-use App\Repository\ClientRepository;
+use App\Entity\AccessToken;
+use App\Repository\AccessTokenRepository;
 
 final class SecurityService
 {
     public function __construct(
-        private ClientRepository $clientRepository
+        private AccessTokenRepository $accessTokenRepository
     )
     {
     }
 
-    public function findClient(
-        string $clientId,
-        string $clientSecret
-    ): ?Client
+    public function generateAccessTokenForCCG(
+        Client $client,
+        string $grantType
+    ): array
     {
-        return $this->clientRepository->findOneBy(['clientId' => $clientId, 'clientSecret' => $clientSecret]);
-    }
+        $token = bin2hex(random_bytes(64));
 
-    public function generateResponsePayloadWithJWT(
-        Client $client
-    ): string
-    {
+        $accessToken = new AccessToken();
 
+        $expire = new DateTime('+1 day');
 
-        return '';
+        $accessToken
+            ->setUser($client->getProject()->getDeveloper()->getUser())
+            ->setAccessToken($token)
+            ->setProject($client->getProject())
+            ->setExpireDate($expire)
+            ->setScopes(['read', 'write', 'modify', 'delete'])
+        ;
+
+        $this->accessTokenRepository->persistAndFlushEntity($accessToken);
+
+        $diff = $expire->diff(new DateTime());
+        $seconds = $diff->s + ($diff->m * 60) + ($diff->h * 3600);
+
+        return [
+            'access_token' => $this->generateJWT($token),
+            'token_type' => 'bearer',
+            'expires_in' => $seconds
+        ];
     }
 
     public function authorizeClientForGrantRestrictedAccess(): bool
@@ -35,6 +52,23 @@ final class SecurityService
 
 
         return true;
+    }
+
+    protected function generateJWT(
+        string $token
+    ): string
+    {
+        $passphrase = '6913abde502dffb25b96af7c5a2e322304d48c89381d852edab6a4e98f343d618e1e8196858ea9193b869a4b50e43d2c65178260dd7a50d89d71cd9394bdcdef';
+        $privateKeyFile = '../private.pem';
+
+        $privateKey = openssl_get_privatekey(
+            file_get_contents($privateKeyFile),
+            $passphrase
+        );
+
+        return JWT::encode([
+            'token' => $token
+        ], $privateKey, 'RS256');
     }
 
 

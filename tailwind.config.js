d322ba4bccedd41321ab722724d5a1ae70a6f035
AC4G <vost_400@gmx.de>
2022-07-27 17:16:09 +0200

    home nav bar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,26 @@ module.exports = {
   content: [
     "./templates/**/*.twig"
   ],
+  variants: {
+
+  },
   theme: {
     extend: {
       colors: {
+        'grey': '#888888',
+      },
+      width: {
 
+      },
+      transitionProperty: {
+        'opacity': 'opacity',
+      },
+      zIndex: {
+        '[-1]': '-1',
       }
+    },
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
     },
   },
   plugins: [],

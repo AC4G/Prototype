b119c366906e62911f57001845113e7b4627a060
AC4G <vost_400@gmx.de>
2022-11-20 15:15:30 +0100

    bug fixed and relevance to searched items added

--- src/Engine/Search/ItemSearchEngine.php
+++ src/Engine/Search/ItemSearchEngine.php
@@ -4,12 +4,16 @@ namespace App\Engine\Search;
 
 use Doctrine\ORM\Query;
 use App\Repository\ItemRepository;
+use App\Service\API\Items\ItemsService;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 final class ItemSearchEngine
 {
+    private array $amountOfOccurrence = [];
+
     public function __construct(
-        private ItemRepository $itemRepository
+        private ItemRepository $itemRepository,
+        private ItemsService $itemsService
     )
     {
     }
@@ -26,10 +30,11 @@ final class ItemSearchEngine
             $parameter = $this->preparePhraseParameter($phraseWithParameter[1]);
 
             $items = $this->buildQuery($phraseWithParameter[0], $user)->execute();
+
             return $this->findItemByParameter($items, $parameter);
         }
 
-        return $this->buildQuery($phrase, $user)->execute();
+        return $this->itemsService->prepareData($this->buildQuery($phrase, $user)->execute());
     }
 
     private function preparePhraseParameter(
@@ -54,22 +59,59 @@ final class ItemSearchEngine
             if (is_string($parameters)) {
                 if (strpos($item->getParameter(), $parameters) > 0) {
                     $foundContent[] = $item;
-                    goto a;
                 }
 
                 continue;
             }
 
             foreach ($parameters as $parameter) {
-                if (strpos($item->getParameter(), $parameter) > 0 && !array_key_exists($item->getId(), $foundContent)) {
-                    $foundContent[$key] = $item;
+                if (strpos($item->getParameter(), $parameter) > 0) {
+                    (array_key_exists($item->getId(), $this->amountOfOccurrence)) ? $this->amountOfOccurrence[$item->getId()] = $this->amountOfOccurrence[$item->getId()] + 1 : $this->amountOfOccurrence[$item->getId()] = 1;
+
+                    if (!array_key_exists($item->getId(), $foundContent)) $foundContent[$item->getId()] = $item;
                 }
             }
         }
 
-        a:
+        if (is_string($parameters)) {
+            return $this->itemsService->prepareData($foundContent);
+        }
 
-        return $foundContent;
+        return $this->filterContentByOccurrenceOfAllPhrases($foundContent, count($parameters));
+    }
+
+    //relevance of the searched items
+    private function filterContentByOccurrenceOfAllPhrases(
+        array $foundContent,
+        int $amountOfParameters
+    ): array
+    {
+        $filteredByRelevance = [];
+        $percentageOfOccurrence = [];
+
+        foreach ($foundContent as $key => $item) {
+            if (array_key_exists($key, $this->amountOfOccurrence)) {
+                $percentageOfOccurrence[$key] = (100 / $amountOfParameters) * $this->amountOfOccurrence[$key];
+            }
+        }
+
+        foreach ($percentageOfOccurrence as $key => $percent) {
+            if ($percent >= 75) {
+                $normalizedItem = $this->itemsService->prepareData($foundContent[$key]);
+                $normalizedItem['relevance'] = $percent;
+                $filteredByRelevance[] = $normalizedItem;
+            }
+        }
+
+        uasort($filteredByRelevance, function ($a, $b) {
+            if ($a['relevance'] === $b['relevance']) {
+                return 0;
+            }
+
+            return ($a['relevance'] < $b['relevance']) ? 1 : -1;
+        });
+
+        return $filteredByRelevance;
     }
 
     private function buildQuery(
@@ -77,7 +119,6 @@ final class ItemSearchEngine
         ?UserInterface $user = null
     ): Query
     {
-
         $query = $this->itemRepository->createQueryBuilder('i')
                 ->where('MATCH (i.name) AGAINST (:phrase IN BOOLEAN MODE) > 0')
                 ->setParameter('phrase', (count(explode(' ', $phrase)) === 1) ? '\'' . $phrase . '*\'' : '"' . $phrase . '"')
@@ -91,4 +132,6 @@ final class ItemSearchEngine
 
         return $query->getQuery();
     }
+
+
 }

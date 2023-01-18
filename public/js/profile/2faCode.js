1fc7640b60aa263ca8fd3c8d8b8c7dfcf1d76c9c
AC4G <vost_400@gmx.de>
2023-01-18 19:26:48 +0100

    copy 2fa recovery tokens added

--- public/js/profile/2faCode.js
+++ public/js/profile/2faCode.js
@@ -1,6 +1,42 @@
 let cannotScanQRCodeButton = document.getElementById('cannot-scan-qr-code');
 let codeTwoFa = document.getElementById('code-2fa');
 
+let twoFaRecoveryTokens = document.querySelectorAll('.token');
+let copiedNotification = document.getElementById('copied');
+
 cannotScanQRCodeButton.addEventListener('click', function () {
     codeTwoFa.classList.remove('hidden')
 });
+
+async function copyRecoveryTokensToClipBoard() {
+    if (window.isSecureContext && navigator.clipboard) {
+        let tokens = '';
+        let counter = 0;
+
+        for (const token of twoFaRecoveryTokens) {
+            counter++;
+
+            tokens += token.textContent;
+
+            if ((counter % 2) === 0) {
+                tokens += '\n';
+                continue;
+            }
+
+            tokens += ' ';
+        }
+
+        await navigator.clipboard.writeText(tokens);
+        showCopiedNotification();
+    }
+}
+
+async function showCopiedNotification() {
+    copiedNotification.style.opacity = '1';
+    await delay(1000);
+    copiedNotification.style.opacity = '0';
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
